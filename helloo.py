7d32d39747fcc612ba2b05125951e44a2454c108
Rohit Singh <dcdc7310@gmail.com>
2026-02-05 14:02:26 +0530

    added dsik usage

--- helloo.py
+++ helloo.py
@@ -1,32 +1,58 @@
-# import psutil
-# import time
-# import os
+import psutil
+import time
+import os
 
-# pid = os.getpid()
-# python_process = psutil.Process(pid)
 
-# print('Memory used by this python script: ',python_process.memory_info()[0]/2**30)
-# cpu_usage = psutil.cpu_percent(interval=1)
-# print(os.name)
-# time.sleep(1)
-# os.system('cls' if os.name=='nt' else 'clear')
-# # print('physical memory useD:',psutil.virtual_memory()[2])
-# # print('physical memory useD:',psutil.virtual_memory().percent)
-# # print(cpu_usage)
+#Memory used by this python script
+pid = os.getpid()
+python_process = psutil.Process(pid)
+print('Memory used by this python script in MB : ',round(python_process.memory_info()[0]/2**20,2),'MB')
 
-# Source - https://stackoverflow.com/a/69511430
-# Posted by Karol Zlot, modified by community. See post 'Timeline' for change history
-# Retrieved 2026-02-05, License - CC BY-SA 4.0
 
-from tqdm import tqdm
-from time import sleep
+#CPU usage
+cpu_usage = psutil.cpu_percent(interval=1)
+print('CPU usage : ',cpu_usage)
+
+
+#Ram usage
+ram_usage = psutil.virtual_memory().percent
+print('RAM usage : ',ram_usage)
+
+disk_usage = psutil.disk_usage('/')
+print('Disk usage : ',disk_usage.percent)
+
+disk_partition = psutil.disk_partitions()
+print('Disk partition : ',disk_partition)
+
+
 import psutil
 
-with tqdm(total=100, desc='cpu%', position=0) as cpubar, tqdm(total=100, desc='ram%', position=1) as rambar:
-    while True:
-        rambar.n = psutil.virtual_memory().percent
-        cpubar.n = psutil.cpu_percent()
-        rambar.refresh()
-        cpubar.refresh()
-        sleep(0.5)
-        break
+for part in psutil.disk_partitions():
+    
+    usage = psutil.disk_usage(part.mountpoint)
+    print(part.device, usage.percent)
+
+
+
+
+time.sleep(1)
+# os.system('cls' if os.name=='nt' else 'clear')
+# print('physical memory useD:',)
+
+
+
+
+#graphiocal bar of the cpu and ram usage
+
+# from tqdm import tqdm
+# from time import sleep
+# import psutil
+
+# with tqdm(total=100, desc='cpu%', position=0) as cpubar, tqdm(total=100, desc='ram%', position=1) as rambar:
+#     while True:
+#         rambar.n = psutil.virtual_memory().percent
+#         cpubar.n = psutil.cpu_percent()
+#         rambar.refresh()
+#         cpubar.refresh()
+#         sleep(0.5)
+#         break
