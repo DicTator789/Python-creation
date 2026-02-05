edd2dcc3a1faf58b9cc3872d13df62c963a241ff
Rohit Singh <dcdc7310@gmail.com>
2026-02-05 15:16:50 +0530

    looging info added

--- helloo.py
+++ helloo.py
@@ -1,38 +1,56 @@
+import logging
 import psutil
 import time
 import os
+import asyncio
 
 
-#Memory used by this python script
-pid = os.getpid()
-python_process = psutil.Process(pid)
-print('Memory used by this python script in MB : ',round(python_process.memory_info()[0]/2**20,2),'MB')
+logging.basicConfig(level=logging.INFO)
 
 
-#CPU usage
-cpu_usage = psutil.cpu_percent(interval=1)
-print('CPU usage : ',cpu_usage)
+async def check(usage):
+    if(usage>80 and usage<90):
+        logging.warning("System is OK")  
+    elif(usage>90):
+        logging.critical("System is critical")
+    else:
+        logging.info("System is OK")
 
+async def main():
+    #Memory used by this python script
+    pid = os.getpid()
+    python_process = round(psutil.Process(pid).memory_info()[0]/2**30,2)
+    print('Memory used by this python script in GB : ',python_process,'GB')
+    await check(python_process)
 
-#Ram usage
-ram_usage = psutil.virtual_memory().percent
-print('RAM usage : ',ram_usage)
+    #CPU usage
+    cpu_usage = psutil.cpu_percent(interval=1)
+    print('CPU usage : ',cpu_usage)
+    await check(cpu_usage)
 
-disk_usage = psutil.disk_usage('/')
-print('Disk usage current drive : ',disk_usage.percent)
+    #Ram usage
+    ram_usage = psutil.virtual_memory().percent
+    print('RAM usage : ',ram_usage)
+    await check(ram_usage)
 
-disk_partition = psutil.disk_partitions()
+    #Disk usage for current drive
+    disk_usage = psutil.disk_usage('/')
+    print('Disk usage current drive : ',disk_usage.percent)
 
-#all the disk usage
-for part in disk_partition:
+    #all the disk usage
+    print('Disk usage All drives : ')
+    disk_partition = psutil.disk_partitions()
+    for part in disk_partition:
+        try:
+            usage = psutil.disk_usage(part.mountpoint)
+            print(part.device, usage.percent)
+            await check(usage.percent)
+        except PermissionError:
+            pass
 
-    usage = psutil.disk_usage(part.mountpoint)
-    print(part.device, usage.percent)
+asyncio.run(main())
 
-
-
-
-time.sleep(1)
+# time.sleep(1)
 # os.system('cls' if os.name=='nt' else 'clear')
 # print('physical memory useD:',)
 
